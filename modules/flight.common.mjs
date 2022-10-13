341f152e8c40dde5154eb910c87846d402212179
Yehonatan Shechter <ydshechter@gmail.com>
2022-10-14 01:01:30 +0300

    02. Creating flight class properties and methods

--- modules/flight.common.mjs
+++ modules/flight.common.mjs
@@ -1,11 +1,62 @@
-import log from '@ajar/marker';
+import log from "@ajar/marker";
 
-// ---------------------------------------------
-// 01. Create and export Flight Class
-// ---------------------------------------------
+/*
+Create and export the Flight Class
+*/  
 
-export default class Flight{
-    constructor(){
-        log.cyan('i am a flight class')
-    }
+export default class Flight {
+	constructor(number, origin, destination, departed, arrived) {
+        // --------------------------------
+		// --== Class properties ==--
+        // --------------------------------
+		this._number = number;
+		this._origin = origin;
+		this._destination = destination;
+		this.departed = departed;
+		this.arrived = arrived;
+
+		log.green("constructor running here!");
+	}
+	// --------------------------------
+	// --== Class Methods ==--
+	// --------------------------------
+	// getters & Setters
+	// 01. number* getter & setter
+	get number() {
+		return this._number;
+	}
+	set number(value) {
+		if (typeof value !== "number") throw new Error("Only numbers are allowed!");
+		if (value < 0 || value > 999)
+			throw new Error("numbers should be between 1-999!");
+		this._number = value;
+	}
+
+	// 02. origin* getter & setter
+	get origin() {
+		return this._origin;
+	}
+	set origin(value) {
+		if (typeof value !== "string") throw new Error("Only strings are allowed!");
+		if (value.length != 3) throw new Error("origin should be a 3-letter-word!");
+		this._origin = value.toUpperCase();
+	}
+
+	// 03. destination* getter & setter
+	get destination() {
+		return this._destination;
+	}
+	set destination(value) {
+		if (typeof value !== "string") throw new Error("Only strings are allowed!");
+		if (value.length != 3) throw new Error("origin should be a 3-letter-word!");
+		this._destination = value.toUpperCase();
+	}
+
+	// private method
+	arrive() {
+		const { destination } = this;
+		return `Hello from ${destination}`;
+	}
+	// public method
+	depart() {}
 };
--- modules/flightmanager.common.mjs
+++ modules/flightmanager.common.mjs
@@ -1,14 +1,21 @@
 import log from '@ajar/marker'; 
-import Flight from '../modules/flight.common.mjs'
+import Flight from '../modules/flight.common.mjs';
 
-// ---------------------------------------------
-// 01. Create and export FlightManager function
-// ---------------------------------------------
+/*
+Create and export the FlightManager function
+*/
 
 export default function() {
 
 	// 01. calling tht imported flight class
-	const myFlight = new Flight
+	const myFlight = new Flight(123, 'arg', 'isr', '08:22', 'no', 'i am a new flight!');
+	// myFlight.number = 156;
+	// myFlight.origin = 'dfv';
+	// myFlight.destination = 'cns';
+
+	log.cyan(myFlight.arrive());
+	log.cyan(myFlight.number);
+	log.cyan(myFlight.destination);
 	
 	return "i am the flight manager";
-}
+};
